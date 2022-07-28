76775f4afda600608c166ab5d7b75de6c0826d77
Sergei <34881448+Mkezhman85@users.noreply.github.com>
2022-07-28 15:01:00 +0300

    Добавлены настройки tsconfig, логирование

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, { Express } from 'express'
+import { Server } from 'http'
+import { ExeptionFilter } from './errors/exeption.filter'
+import { LoggerService } from './logger/logger_service'
+import { UserController } from './users/users_controller'
+
+export class App {
+
+	app: Express
+	server: Server
+	port: number
+	logger: LoggerService
+	userController: UserController
+	exeptionFilter: ExeptionFilter
+
+	constructor(
+		logger: LoggerService,
+		userController: UserController,
+		exeptionFilter: ExeptionFilter
+		) {
+		this.app = express()
+		this.port = 8000
+		this.logger = logger
+		this.userController = userController
+		this.exeptionFilter = exeptionFilter
+	}
+
+	useRoutes() {
+		this.app.use('/users', this.userController.router)
+	}
+
+	useExeptionFilter() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
+	}
+
+	public async init() {
+		this.useRoutes()
+		this.useExeptionFilter()
+		this.server = this.app.listen(this.port)
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`)
+		// console.log(`Сервер запущен на http://localhost:${this.port}`)
+	}
+}
